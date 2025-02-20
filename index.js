57b8e5c0b8aca2a4a03a9095fecd2b4d80ebeff5
shohel-ahmed <shohelahmed27.sa@gmail.com>
2025-02-21 01:28:13 +0800

    logic done

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 
 function highestScore(score){
-
-
+    var max=score[0];
+    for (var x=1; x<score.length;x++){
+        if (max<score[x]){
+            max=score[x];
+        }
+    }
+    return max;
 
 }
 
 var num =[1,56,87,35,8];
-highestScore(num);
+var maxScore=highestScore(num);
+document.write(maxScore);
